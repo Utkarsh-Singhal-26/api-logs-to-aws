a4a2f811c07b23ce635c8c227d8abaea1bce9b8b
Utkarsh-Singhal-26 <singhalutkarsh26@gmail.com>
2024-10-02 13:17:00 +0530

    [ADDED : INTERCEPTED XHR REQUEST]

--- extension/intercept.js
+++ extension/intercept.js
@@ -1,6 +1,8 @@
 (function () {
   const originalFetch = window.fetch;
+  const originalXHR = window.XMLHttpRequest;
 
+  // Intercept Fetch requests
   window.fetch = function (...args) {
     const url = typeof args[0] === "string" ? args[0] : args[0].url;
 
@@ -13,9 +15,7 @@
             type: "Fetch",
             method: args[1]?.method || "GET",
             url,
-            requestHeaders: args[1]?.headers
-              ? Object.fromEntries(args[1].headers)
-              : {},
+            requestHeaders: args[1]?.headers || {},
             requestBody: args[1]?.body || "",
             responseBody,
             status: response.status,
@@ -32,5 +32,62 @@
         });
       return response;
     });
+  };
+
+  // Intercept XHR requests
+  window.XMLHttpRequest = function () {
+    const xhr = new originalXHR();
+    const originalOpen = xhr.open;
+    const originalSend = xhr.send;
+    const originalSetRequestHeader = xhr.setRequestHeader;
+
+    xhr.open = function (method, url) {
+      this._url = url;
+      this._method = method;
+      this._requestHeaders = {};
+      return originalOpen.apply(this, arguments);
+    };
+
+    xhr.setRequestHeader = function (header, value) {
+      this._requestHeaders[header] = value;
+      return originalSetRequestHeader.apply(this, arguments);
+    };
+
+    xhr.send = function (body) {
+      this.addEventListener("load", () => {
+        const responseHeaders = {};
+        this.getAllResponseHeaders()
+          .split("\r\n")
+          .forEach((line) => {
+            if (line) {
+              const [key, value] = line.split(": ");
+              responseHeaders[key] = value;
+            }
+          });
+        const requestData = {
+          type: "XHR",
+          method: this._method,
+          url: this._url,
+          requestHeaders: this._requestHeaders,
+          requestBody: body,
+          responseHeaders: responseHeaders,
+          responseBody: this.responseText,
+          status: this.status,
+          statusText: this.statusText,
+        };
+
+        window.postMessage(
+          {
+            type: "INTERCEPTED_REQUEST",
+            data: requestData,
+          },
+          "*"
+        );
+      });
+
+      return originalSend.apply(this, arguments);
+    };
+
+    return xhr;
   };
 })();
